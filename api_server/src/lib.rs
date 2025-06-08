00bd241adcf0138e2214b816cfc3da630e3c8a2e
KY00KIM <dev.kyoomin@gmail.com>
2025-06-09 01:45:20 +0900

    Move APIServer module to lib

--- api_server/src/lib.rs
+++ api_server/src/lib.rs
@@ -1,3 +1,72 @@
 pub mod args;
 pub mod pb;
 pub mod router;
+
+use pb::llm::GenerateRequest;
+use router::EngineRouter;
+use serde::{Deserialize, Serialize};
+use tokenizers::tokenizer::{Result, Tokenizer};
+
+#[derive(Serialize)]
+pub struct GenerateOutput {
+    pub session_id: String,
+    pub token_ids: Vec<u32>,
+    pub output_text: String,
+    pub output_len: usize,
+}
+
+#[derive(Deserialize)]
+pub struct GenerateParams {
+    pub prompt: String,
+    pub num_samples: u16,
+    pub session_id: Option<String>,
+    pub max_output_len: Option<u64>,
+    pub ignore_eos: bool,
+}
+
+pub struct APIServer {
+    pub router: EngineRouter,
+    pub tokenizer: Tokenizer,
+}
+
+impl APIServer {
+    pub fn new(router: EngineRouter, tokenizer: Tokenizer) -> Self {
+        Self { router, tokenizer }
+    }
+
+    pub async fn generate(&self, params: GenerateParams) -> Result<GenerateOutput> {
+        // If no session ID is given, generate a new one
+        let session_id = params
+            .session_id
+            .unwrap_or(utils::random::generate_session_id());
+
+        let input_ids = self
+            .tokenizer
+            .encode(params.prompt, false)?
+            .get_ids()
+            .to_vec();
+
+        let response = self
+            .router
+            .generate(GenerateRequest {
+                session_id: session_id,
+                input_ids: input_ids.clone(),
+                num_samples: params.num_samples as u32,
+                max_output_len: params.max_output_len,
+                ignore_eos: params.ignore_eos,
+            })
+            .await?;
+
+        let session_id = response.session_id;
+        let output_ids = response.output_ids;
+        let output_text = self.tokenizer.decode(&output_ids, false)?;
+        let output_len = output_ids.len();
+
+        Ok(GenerateOutput {
+            session_id,
+            token_ids: [input_ids, output_ids].concat(),
+            output_text,
+            output_len,
+        })
+    }
+}
--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -2,77 +2,12 @@ use std::sync::Arc;
 
 use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::post};
 use clap::Parser;
-use serde::{Deserialize, Serialize};
 use tokenizers::tokenizer::{Result, Tokenizer};
 use tracing::info;
 
 use api_server::args::APIServerArgs;
-use api_server::pb::llm::GenerateRequest;
 use api_server::router::EngineRouter;
-
-#[derive(Serialize)]
-struct GenerateOutput {
-    session_id: String,
-    token_ids: Vec<u32>,
-    output_text: String,
-    output_len: usize,
-}
-
-#[derive(Deserialize)]
-struct GenerateParams {
-    prompt: String,
-    num_samples: u16,
-    session_id: Option<String>,
-    max_output_len: Option<u64>,
-    ignore_eos: bool,
-}
-
-struct APIServer {
-    router: EngineRouter,
-    tokenizer: Tokenizer,
-}
-
-impl APIServer {
-    fn new(router: EngineRouter, tokenizer: Tokenizer) -> Self {
-        Self { router, tokenizer }
-    }
-
-    async fn generate(&self, params: GenerateParams) -> Result<GenerateOutput> {
-        // If no session ID is given, generate a new one
-        let session_id = params
-            .session_id
-            .unwrap_or(utils::random::generate_session_id());
-
-        let input_ids = self
-            .tokenizer
-            .encode(params.prompt, false)?
-            .get_ids()
-            .to_vec();
-
-        let response = self
-            .router
-            .generate(GenerateRequest {
-                session_id: session_id,
-                input_ids: input_ids.clone(),
-                num_samples: params.num_samples as u32,
-                max_output_len: params.max_output_len,
-                ignore_eos: params.ignore_eos,
-            })
-            .await?;
-
-        let session_id = response.session_id;
-        let output_ids = response.output_ids;
-        let output_text = self.tokenizer.decode(&output_ids, false)?;
-        let output_len = output_ids.len();
-
-        Ok(GenerateOutput {
-            session_id,
-            token_ids: [input_ids, output_ids].concat(),
-            output_text,
-            output_len,
-        })
-    }
-}
+use api_server::{APIServer, GenerateParams};
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> Result<()> {
